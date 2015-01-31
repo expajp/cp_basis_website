140864f473bc6ce84f1eb9c49ea5b20006cb1a89
Shu OGAWARA <expajp@gmail.com>
2015-01-31 17:47:08 +0900

    著作権表示の西暦を自動変更

--- js/base.js
+++ js/base.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    
+    /*著作権表示を今年に書き換え*/
+    var this_year = $("#this_year");
+    var fullyear = new Date().getFullYear();
+    if(fullyear > 2015) this_year.html(fullyear);
+    
+    /*Pagetopのアニメーション化*/
     var pagetop = $("#pagetop");
     pagetop.click(function(){
         $('body, html').animate({ scrollTop: 0 }, 500);

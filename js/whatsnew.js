76bf69e95fc7314292e30f4eb0b92bf4f3bf601a
Shu OGAWARA <expajp@gmail.com>
2015-02-19 22:00:20 +0900

    途中経過2

--- js/whatsnew.js
+++ js/whatsnew.js
@@ -10,8 +10,9 @@ var today = new Date();
 var year = today.getFullYear(), month = today.getMonth() + 1, day = today.getDate();
 
 /* 定数の設定 */
-var filename = "whatsnew.csv";/* データファイルの場所 */
-var LF = String.fromCharCode(10);/* 改行コードをLFに指定 これでCR+LFでも区切れる */
+var filename = "js/whatsnew.txt";/* データファイルの場所 */
+var LF = String.fromCharCode(10);/* 改行コードをLFに指定 これでLF+LFでも区切れる */
+var startYear = 2015;/*データの存在する最初の年*/
 
 
 // 関数定義ここから
@@ -61,14 +62,16 @@ function toArrayfromLine(line){
 
 
 /* ページ読み込みと同時に実行 */
-$(function(){
+$(document).ready(function(){
     
     /* お知らせの配列 */
-    var ableToSeeInIndex = new Array();/* index.htmlに掲載するもの */
-    var archives = new Array();/* 過去のお知らせを年ごとに格納した配列の配列 */
+    var ableToSeeInIndex = $("#whatsnew");/* index.htmlの更新情報のパネル */
+    var panels = new Array();/* 年ごとのパネルを格納した配列 */
     
-    /* 2015年から今年まで、年ごとに配列を作成 */
-    for(i=2015;i<=year;i++) archives[i] = new Array();
+    /* 2015年から今年まで、年ごとにパネルを作成 */
+    for(i=startYear;i<=year;i++){
+        panels[i] = $("<div>").addClass("panel");
+    }
 
     /* ファイルを行ごとに分ける */
     var text = getTextFile(filename);
@@ -76,42 +79,58 @@ $(function(){
 
     /* データを1行ずつ読みだして文字列へ変換し、時期により配列へ格納 */
     for(var i=0;i<line.length;i++){
+        
         /* データ読み出し */
         var data = toArrayfromLine(line[i]);
 
         /* 要素作成 */
-        //TODO
-        var parentDiv = document.createElement("div");
+        var parentDiv = $("<div>").addClass("row");
         
-        var dateDiv = document.createElement("div");
-        dateDiv.addClass("large-2 medium-3 small-5 colums");
-        dateDiv.id = "date";
+        var dateDiv = $("<div>").addClass("large-2 medium-3 small-5 columns")
+                                .attr("id", "date");
         
-        var link = document.createElement("a");
-        var str = new String("Date");
+        var contentDiv = $("<div>").addClass("large-10 medium-9 small-7 columns")
+                                   .attr("id", "content");
+        
+        /* 日付用の文字列を作成し、dateDivの子として追加 */
+        var str = data[0] + '.' + data[1] + '.' + data[2];
+        dateDiv.append(str);
+        
+        /* 詳細をcontentDivの子として追加 */
+        contentDiv.append(data[3]);
 
-        /* 日付用の文字列を作成 */
-        str = data[0] + '.' + data[1] + '.' + data[2];
+        /* dateDivとcontentDivをparentDivの子として追加 */
+        parentDiv.append(dateDiv).append(contentDiv);
 
-        /* 文字列をaタグの子として追加 */
-        link.appendChild(document.createTextNode(str));
-
-        /* 作成したリンクをliタグの要素として登録 */
-        item.appendChild(link);
-
-        /* 隔年ごとの配列に格納 */
-        archives[data[0]].push(item);
+        /* 年ごとのパネルに子要素として格納 */
+        panels[data[0]].append(parentDiv);
+        
+        /* 最新の3件はindex.htmlからでも見られるようにする */
+        if(i<3) ableToSeeInIndex.append(parentDiv);
         
     }
     
     /* HTMLの書き換え */
-    <a name="2015"></a>  
-                <h4>2015年</h4>
-                <div class="panel">
-                    <div class="row">                        
-                        <div class="large-2 medium-3 small-5 columns" id="date">2015.02.xx</div>
-                        <div class="large-10 medium-9 small-7 columns" id="content">研究室の統合にあわせてサイトを一新しました。</div>                        
-                    </div>
-                </div>
+    for(i=startYear;i<=year;i++){
+        /*各年ごとの見出しを作成して追加*/
+        var eachYearMilestone = document.createElement("a");
+        eachYearMilestone.setAttribute("name", i);
+        
+        var eachYearHeadline = document.createElement("h4");
+        eachYearHeadline.appendChild(document.createTextNode(i+"年"));
+        
+        $("#archive_content").append(eachYearMilestone);
+        $("#archive_content").append(eachYearHeadline);
+        $("#archive_content").append(panels[i]);
+        
+        /*インデックスに各年へのページ内リンクを作成*/
+        var index = document.createElement("li");
+        var link = document.createElement("a");
+        link.setAttribute("href", '#'+i);
+        index.appendChild(link);
+        
+        $("#archives_index").append(index);
+    }    
+
 
 }); 

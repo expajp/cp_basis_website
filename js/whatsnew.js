51c14dc9d7cb051786bdc1a2666a5612ed709a0d
Shu OGAWARA <expajp@gmail.com>
2015-02-25 02:22:57 +0900

    更新履歴の記入先を一本化
    fix #7

--- js/whatsnew.js
+++ js/whatsnew.js
@@ -10,33 +10,13 @@ var today = new Date();
 var year = today.getFullYear(), month = today.getMonth() + 1, day = today.getDate();
 
 /* 定数の設定 */
-var filename = "js/whatsnew.txt";/* データファイルの場所 */
-var LF = String.fromCharCode(10);/* 改行コードをLFに指定 これでLF+LFでも区切れる */
-var startYear = 2015;/*データの存在する最初の年*/
+var filename = "js/whatsnew.csv";/* データファイルの場所 */
+var LF = String.fromCharCode(10);/* 改行コードをLFに指定 */
+var startYear = 2015;/* データの存在する最初の年 */
+var maxLinesInIndex = 5;/* index.htmlに表示するお知らせの数 */
 
 
 // 関数定義ここから
-
-/* テキストの読み込み */
-function getTextFile(fname) {
-
-    /* 変数の定義 */
-    var text = null; /* 読み込むテキスト */
-    var ajax = new XMLHttpRequest();/* ajaxを使うためのオブジェクト */
-
-    with (ajax) {
-        /* テキストが読み込まれてからメソッドを実行 */
-        onload = function () { readyState == 4 && status == 200 && (text = responseText); };
-
-        /* ファイル読み込み、ファイルの読み込みの成否にかかわらず次を実行 */
-        open('GET', fname, false);
-
-        /* 何も送信しない。いわゆるおまじない */
-        send(null);
-    };
-
-    return text;
-}
 
 /* テキストを行ごとに配列へ格納 */
 function toLinefromText(text){
@@ -58,6 +38,21 @@ function toArrayfromLine(line){
     return data;
 }
 
+/* ページ内リンクをアニメーションにする */
+function animateScroll(){
+    var speed = 500;
+    var href= $(this).attr("href");
+
+    var parts = href.split("#");
+    var target = parts[1];
+
+    var position = Math.max($('a[name='+target+']').offset().top-45, 0);
+
+    /*スクロール*/
+    $('body, html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+}
+
 // 関数定義ここまで
 
 
@@ -72,65 +67,87 @@ $(document).ready(function(){
     for(i=startYear;i<=year;i++){
         panels[i] = $("<div>").addClass("panel");
     }
-
-    /* ファイルを行ごとに分ける */
-    var text = getTextFile(filename);
-    var line = toLinefromText(text);
-
-    /* データを1行ずつ読みだして文字列へ変換し、時期により配列へ格納 */
-    for(var i=0;i<line.length;i++){
-        
-        /* データ読み出し */
-        var data = toArrayfromLine(line[i]);
-
-        /* 要素作成 */
-        var parentDiv = $("<div>").addClass("row");
-        
-        var dateDiv = $("<div>").addClass("large-2 medium-3 small-5 columns")
-                                .attr("id", "date");
-        
-        var contentDiv = $("<div>").addClass("large-10 medium-9 small-7 columns")
-                                   .attr("id", "content");
-        
-        /* 日付用の文字列を作成し、dateDivの子として追加 */
-        var str = data[0] + '.' + data[1] + '.' + data[2];
-        dateDiv.append(str);
-        
-        /* 詳細をcontentDivの子として追加 */
-        contentDiv.append(data[3]);
-
-        /* dateDivとcontentDivをparentDivの子として追加 */
-        parentDiv.append(dateDiv).append(contentDiv);
-
-        /* 年ごとのパネルに子要素として格納 */
-        panels[data[0]].append(parentDiv);
-        
-        /* 最新の3件はindex.htmlからでも見られるようにする */
-        if(i<3) ableToSeeInIndex.append(parentDiv);
-        
-    }
     
-    /* HTMLの書き換え */
-    for(i=startYear;i<=year;i++){
-        /*各年ごとの見出しを作成して追加*/
-        var eachYearMilestone = document.createElement("a");
-        eachYearMilestone.setAttribute("name", i);
-        
-        var eachYearHeadline = document.createElement("h4");
-        eachYearHeadline.appendChild(document.createTextNode(i+"年"));
-        
-        $("#archive_content").append(eachYearMilestone);
-        $("#archive_content").append(eachYearHeadline);
-        $("#archive_content").append(panels[i]);
-        
-        /*インデックスに各年へのページ内リンクを作成*/
-        var index = document.createElement("li");
-        var link = document.createElement("a");
-        link.setAttribute("href", '#'+i);
-        index.appendChild(link);
-        
-        $("#archives_index").append(index);
-    }    
+    /* index.htmlの表示領域の初期化 */
+    ableToSeeInIndex.html("");
+    
+    /* 最新のn件だけindex.htmlに表示していることをお知らせ */
+    $("#max_new").html(maxLinesInIndex);
+    
+    /* データを取得し、成功したらその後の処理を行う */
+    $.get(filename, function(data, status, xhr){
+        if(status == "success"){
+            text = data;            
+            var line = toLinefromText(text);
+            
+            /* データを1行ずつ読みだして文字列へ変換し、時期により配列へ格納 */
+            for(var i=0;i<line.length-1;i++){
 
+                /* データ読み出し */
+                var data = toArrayfromLine(line[i]);
+
+                /* 要素作成 */
+                var parentDiv = $("<div>").addClass("row");
+
+                var dateDiv = $("<div>").addClass("large-2 medium-3 small-5 columns")
+                                        .attr("id", "date");
+
+                var contentDiv = $("<div>").addClass("large-10 medium-9 small-7 columns")
+                                           .attr("id", "content");
+
+                /* 日付用の文字列を作成し、dateDivの子として追加 */
+                var str = data[0] + '.' + data[1] + '.' + data[2];
+                dateDiv.html(str);
+
+                /* 詳細をcontentDivの子として追加 */
+                contentDiv.html(data[3]);
+
+                /* dateDivとcontentDivをparentDivの子として追加 */
+                parentDiv.append(dateDiv);
+                parentDiv.append(contentDiv);
+
+                /* 年ごとのパネルに子要素として格納 */
+                panels[data[0]].append(parentDiv);
+
+                /* 最新の{maxLinesInIndex}件はindex.htmlからでも見られるようにする */
+                if(i < maxLinesInIndex) ableToSeeInIndex.append(parentDiv);
+                
+                /* 次の要素が存在するかチェック */
+                if(line[i+1] == null) break;
+
+            }
+
+            /* HTMLの書き換え */
+            
+            /* 書き換え領域の初期化 */
+            $("#archive_content").html("");
+            $("#archive_index ul.disc").html("");
+            
+            for(i=year;i>=startYear;i--){
+                
+                /* 各年ごとの見出しを作成 */
+                var eachYearMilestone = $("<a>").attr("name", i);
+                var eachYearHeadline = $("<h4>").text(i+"年");
+
+                /* 作成した見出しと作成済みのパネルを本文に追加 */
+                $("#archive_content").append(eachYearMilestone);
+                $("#archive_content").append(eachYearHeadline);
+                $("#archive_content").append(panels[i]);
+
+                /* インデックスに各年へのページ内リンクを作成 */
+                var index = $("<li>");
+                var link = $("<a>").attr("href", '#'+i).text(i);
+                
+                /* スクロールにアニメーションをつける*/
+                link.click(animateScroll);
+                
+                /* リンクをインデックスに追加し、本文に反映 */
+                index.append(link);
+                $("#archives_index ul.disc").append(index);
+            }
+            
+        }
+    });
+    
 
 }); 

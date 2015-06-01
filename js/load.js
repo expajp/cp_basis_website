53d5ca3795036eaf27f8a46684fcb75477812e16
Shu OGAWARA <expajp@gmail.com>
2015-06-01 18:03:31 +0900

    delete load.js

--- js/load.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-            
-            $(".breadcrumbs").css("display", "block"); /* パンくずリスト */
-            $("#main div").css("display", "block"); /* コンテンツ要素 */
-    
-            $("#main").css("height", "auto"); /* コンテンツ表示領域の高さを元に戻す */
-    
-}
